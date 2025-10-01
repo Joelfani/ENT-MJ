a2e263975a5faa5506fb2a2e4623196d5bb2ba91
Joel <tsarafaniry11@gmail.com>
2025-10-01 06:42:24 +0300

    mise a jour vue.config.js pour vercel

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  //publicPath: process.env.NODE_ENV === 'production' ? './' : '/' //avant sur vercel 
+  publicPath: '/'
 })
